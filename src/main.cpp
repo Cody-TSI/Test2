17dd22685fc8f868d6bff61c937bae3293f3739c
Cody Barzey-Francois <cbfrancois@softwareinstitute.com>
2022-04-01 11:59:44 +0100

    added feature

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int moreFeatureCode()
+{
+    std::cout << "hello World" << std::endl;
+    return 0;
+}
+
+int mainBranchDevelopment_2()
+{
+    std::cout << "hello World" << std::endl;
+    return 0;
+}
+
+int mainBranchDevelopment_1()
+{
+    std::cout << "hello World" << std::endl;
+    return 0;
+}
+
+int newFeatureFromMainBranch()
+{
+    std::cout << "hello World" << std::endl;
+    return 0;
+}
+
+int feature1UICODE()
+{
+    std::cout << "hello World" << std::endl;
+    return 0;
+}
+
+int main()
+{
+    std::cout << "hello World" << std::endl;
+    return 0;
+}
