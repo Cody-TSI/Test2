2628d8b63edc26484844676d7ee6c9a10e08fdfa
Cody Barzey-Francois <cbfrancois@softwareinstitute.com>
2022-04-01 12:06:25 +0100

    added user input

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-
+#include <string>
 int main()
 {
-    std::cout << "hello World" << std::endl;
+    std::cout << "please input a string" << std::endl;
+    std::string input;
+    std::cin >> input;
+
+    std::cout << input << std::endl;
     return 0;
 }
