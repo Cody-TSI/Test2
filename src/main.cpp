eb33146c31064c167447765585ef7f19114cec32
Cody Barzey-Francois <cbfrancois@softwareinstitute.com>
2022-04-01 12:24:46 +0100

    added user name

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,11 @@ int main()
     std::cin >> input;
 
     std::cout << input << std::endl;
+
+    std::cout << "please input a age" << std::endl;
+    int age;
+    std::cin >> age;
+
+    std::cout << age << std::endl;
     return 0;
 }
